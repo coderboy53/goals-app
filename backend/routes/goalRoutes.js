655dcae226af783fb70cd66ba74b756d24ae503f
Soham Mitra <mitra.soham53@gmail.com>
2023-01-15 23:01:12 +0530

    Shortened route functions, imported controller functions

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,12 +2,18 @@ const express = require('express')
 const router = express.Router();
 const {getGoals, setGoals, updateGoals, deleteGoals} = require("../controllers/goalController");
 
-router.get('/', getGoals)
 
-router.post('/', setGoals)
 
-router.put('/:id', updateGoals)
+// router.get('/', getGoals)
 
-router.delete('/:id', deleteGoals)
+// router.post('/', setGoals)
+// to reduce LOC can be reduced to 
+router.route('/').get(getGoals).post(setGoals);
+
+// router.put('/:id', updateGoals)
+
+// router.delete('/:id', deleteGoals)
+// to reduce LOC can be reduced to
+router.route('/:id').put(updateGoals).delete(deleteGoals);
 
 module.exports = router
