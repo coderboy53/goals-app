1c636bf0322e5758c6154938e1ab1e34d666454b
Soham Mitra <mitra.soham53@gmail.com>
2023-01-15 23:00:23 +0530

    Updated controllers

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,19 +1,40 @@
-const getGoals = (req, res) => {
+const asyncHandler = require('express-async-handler')
+// @desc Get goal
+// @route GET /api/goals
+// @access Private
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Get goals'});
-}
+})
 
-const setGoals = (req, res) => {
+// @desc Set goal
+// @route POST /api/goals
+// @access Private
+const setGoals = asyncHandler(async (req, res) => {
+    if(!req.body.text)
+    {
+        res.status(400);
+        throw new Error("Please add a text field");
+    }
     res.status(200).json({message: 'Set goals'});
-}
+})
 
-const updateGoals = (req, res) => {
+// @desc Update goal
+// @route PUT /api/goals/:id
+// @access Private
+const updateGoals = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Update goal ${req.params.id}`});
-}
+})
 
-const deleteGoals = (req, res) => {
+// @desc Delete goal
+// @route DELETE /api/goals/:id
+// @access Private
+const deleteGoals = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Delete goal ${req.params.id}`});
-}
+})
 
+// @desc Get goal
+// @route GET /api/goals
+// @access Private
 module.exports = {
     getGoals, setGoals, updateGoals, deleteGoals
 }
