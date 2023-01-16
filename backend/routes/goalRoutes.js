9508e575090362a8e029e86433886b69e7734c6c
Soham Mitra <mitra.soham53@gmail.com>
2023-01-16 22:04:45 +0530

    Added JWT authentication to goal fetching for individual users

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,18 +2,18 @@ const express = require('express')
 const router = express.Router();
 const {getGoals, setGoals, updateGoals, deleteGoals} = require("../controllers/goalController");
 
-
+const { protect } = require('../middleware/authMiddleware')
 
 // router.get('/', getGoals)
 
 // router.post('/', setGoals)
 // to reduce LOC can be reduced to 
-router.route('/').get(getGoals).post(setGoals);
+router.route('/').get(protect, getGoals).post(protect, setGoals);
 
 // router.put('/:id', updateGoals)
 
 // router.delete('/:id', deleteGoals)
 // to reduce LOC can be reduced to
-router.route('/:id').put(updateGoals).delete(deleteGoals);
+router.route('/:id').put(protect, updateGoals).delete(protect, deleteGoals);
 
 module.exports = router
